f0d893c0c8cb1d2ac79af3f58a45dbb3fd809089
Gabriel <57547790+programacaogabriel@users.noreply.github.com>
2023-08-09 15:09:59 -0300

    :construction: Trabalhos e Carrosel
    adicionado imagem em trabalho e feito carrosel para postar fotos

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/* Menu mobile */
+
 function openNav(params) {
     document.getElementById("closed").style.width="100%"
 }
@@ -6,4 +8,29 @@ function closeNav(params) {
     document.getElementById("closed").style.width="0%"
 }
 
+/* Troca de imagens*/
+
+var number_img = 1;
+
+function change_img_add(){
+    var img = document.getElementById("img1")
+
+    if(number_img < 3){
+    number_img += 1;
+    }else {
+        number_img = 3
+    }
+    img.src= "./assets/img" + number_img +".jpeg"
+}
+
+function change_img_remove(){
+    var img = document.getElementById("img1")
+
+    if(number_img > 1){
+    number_img -= 1;
+    }else {
+        number_img = 1
+    }
+    img.src= "./assets/img" + number_img +".jpeg"
+}
 
